63c536eb1a4435f6a6a904dddfc2c1a2f223d0bf
Joshua Powell <joshua@developedsimple.com>
2012-03-27 10:27:46 -0400

    Update the Developed Simple primary site structure

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html> 
+
+<html lang="en"> 
+
+  <head> 
+
+    <meta charset="UTF-8" />
+    
+    <meta name="description" content="We plan, design, &amp; build websites with integrated content &amp; data management systems for government, healthcare, and nonprofit organizations." />
+    <meta name="keywords" content="content, data, management, systems, aggregations, migrations, social, media, email, marketing, campaign, website, application, design, development, resources, drupal, wordpress, expression, engine" /> 
+    
+    <meta name="google-site-verification" content="oa_EvGkh8HrMOppEaBEvuyisyIkskmAFL7J1f83mstM" />
+    <meta name="msvalidate.01" content="7BA8EC0E18618FC5D15FF61642A71E0A" />
+        
+    <meta name="robots" content="index,follow,archive" /> 
+    
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+    <link rel="stylesheet" media="all" href="http://www.developedsimple.com/css/global.css" /> 
+    <link type="text/plain" rel="author" href="http://www.developedsimple.com/humans.txt" />
+    
+    <title>Content &amp; Data Management, Aggregation, &amp; Migration</title> 
+  
+    <!--[if lt IE 9]>
+      <script src="http://www.developedsimple.com/js/html5.js"></script>
+    <![endif]-->
+  </head> 
+  
+  <body> 
+  
+  <div id="wrapper">
+   
+    <section id="header"> 
+      <header>
+        <a id="logo" href="http://www.developedsimple.com/" title="Go to the Developed Simple home page">Developed Simple</a> 
+      </header>
+    </section>
+        
+    <section id="content">
+      <article>
+        <h2>Developed Simple builds websites for organizations like AIA Pittsburgh, <a title="Visit the website we helped MAYA build for Rhiza Labs" href="http://www.rhiza.com/" rel="nofollow" target="_blank">Rhiza</a>, <a title="Visit the website we helped Little Kelpie build for Riverlife Pittsburgh" href="http://riverlifepgh.org/" rel="nofollow" target="_blank">Riverlife Pittsburgh</a>, <a title="Visit the website we helped Little Kelpie build for the Garden Theater Block" href="http://gardentheaterblock.com/" rel="nofollow" target="_blank">Garden Theater Block</a>, <a title="Visit the website we designed &amp; built for Project to End Human Trafficking" href="http://www.endhumantrafficking.org/" target="_blank">Project to End Human Trafficking</a>, <a href="http://heinz.cmu.edu/" rel="nofollow" target="_blank">Heinz College</a>, <a href="http://www.nrel.gov/" title="National Renewable Energy Laboratory" rel="nofollow" target="_blank">NREL</a>, <a href="http://www.healthcostinstitute.org/" rel="nofollow" target="_blank">HCCI</a>, <a href="http://www.kenyonreview.org" rel="nofollow" target="_blank">Kenyon Review</a>, <a href="http://www.pghaeyc.org/" rel="nofollow" target="_blank">PAEYC</a>, &amp; <a href="http://wqed.org/" rel="nofollow" target="_blank">WQED</a>.</h2> 
+        <h2>We also help the <a href="http://www.delallo.com/" rel="nofollow" target="_blank">George E. DeLallo Co.</a>, <a href="http://www.quantumtheatre.com" rel="nofollow" target="_blank">Quantum Theatre</a>, the <a href="http://www.partisanproject.org/" rel="nofollow" target="_blank">Partisan Project</a>, <a href="http://www.riversofsteel.com/map" rel="nofollow" target="_blank">Steel Industry Heritage Corporation</a>, University of Pittsburgh, &amp; <a href="http://www.wyep.org/" rel="nofollow" target="_blank">WYEP</a> maintain their websites &amp; promote their content through email marketing, social media, online video, &amp; dynamic mapping.</h2> 
+        <p>We can also help you perform large scale content migrations, aggregate your third party content (e.g., Facebook, Twitter), build tools for managing and growing online communites, building websites that integrate with mapping services, and promoting your endevours with integrated email marketing campaigns and making sure that your visual branding is consistent across the internet.</p>
+        <p>
+          If you think you'd like working with us, then <a href="tel:+14129563358">give us a call</a>, <a href="mailto:connect+website@developedsimple.com">send us an email</a>, or schedule a time to come over and visit us.
+        </p>
+      </article>
+    </section> 
+            
+    <section id="footer"> 
+      <article class="column first">
+        <h5 class="column-title">Come meet us:</h5> 
+        <p>
+          6014 Penn Avenue<br /> 
+          Pittsburgh, PA<br /> 
+          15206
+        </p>
+      </article> 
+ 
+      <article class="column"> 
+        <h5 class="column-title">Give us a call:</h5>
+        <p>(412) 956-3358<br /><br /></p>
+
+        <h5 class="column-title">Send us a fax:</h5> 
+        <p>
+          You're kidding ... right?
+        </p>
+      </article> 
+ 
+      <article class="column"> 
+        <h5 class="column-title">Connect with us online:</h5> 
+        <ul>
+          <li><a href="http://twitter.com/developedsimple" target="_blank">Follow us on Twitter</a></li>
+          <li><a href="http://www.facebook.com/developedsimple/" target="_blank">Like us on Facebook</a></li>
+          <li><a href="https://plus.google.com/117368369230508125817" target="_blank">Find us on Google+</a></li>
+          <li><a href="http://www.linkedin.com/company/developed-simple" target="_blank">Follow us on LinkedIn</a></li>
+        </ul>       
+        
+      </article> 
+ 
+ 
+      <article class="column last"> 
+        <h5 class="column-title">Jobs:</h5>
+        <ul>
+          <li>
+            <a href="http://www.developedsimple.com/jobs/front-end-development-intern.html">Front-End Development Intern</a>
+          </li> 
+          <li>
+            <a href="http://www.developedsimple.com/jobs/back-end-development-intern.html">Back-End Development Intern</a>
+          </li> 
+        </ul>
+      </article> 
+    </section> 
+ 
+    <script type="text/javascript">
+    
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-3044060-2']);
+      _gaq.push(['_trackPageview']);
+    
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    
+    </script>
+
+  </div>
+
+  </body> 
+ 
+</html>  
